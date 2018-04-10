a2a580662490df7f2530ec64e614baac57810701
jßan <ahsan.wtc@gmail.com>
2018-04-10 17:36:52 +0200

    finishing code

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
+import Gift from './Gift';
 
 class App extends Component {
     state = {
@@ -14,6 +15,11 @@ class App extends Component {
         this.setState({ gifts });
     }
 
+    removeGift = (id) => {
+        const gifts = this.state.gifts.filter(gift => gift.id !== id);
+        this.setState({ gifts });
+    }
+
     render() {
         return (
             <div>
@@ -21,7 +27,7 @@ class App extends Component {
                 <div className="gift-list">
                     {this.state.gifts.map(gift => {
                         return (
-                            <div key={gift.id}></div>
+                            <Gift key={gift.id} gift={gift} removeGift={this.removeGift}></Gift>
                         )
                     })}
                 </div>
